29b3c7d1bf123d2b9e5cf3a65e45d27e02712b74
Your Name <kalaitzidispaul@gmail.com>
2022-02-11 22:28:38 -0500

    added style sheet

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,23 @@
 var timerEl = document.querySelector('.timer')
 var questionsEl = document.querySelector('.questions')
-var questionNumberEl = document.querySelector('#')
+// var questionNumberEl = document.querySelector('#')
+var questionNumbers = 0;
+
+
+//timer 75 seconds
+var timerCountdown = 75;
+
+// timer countdwon use countdown method
+var countdown = function(timerCountdown) {
+    for(var i = timerCountdown; i > 0; i--){
+        console.log(i);
+    }
+}
+
+//function that will hold generated questions
+var generateQuestion = function(){
+    var questionContainerEl = document.createElement("div");
+    questionContainerEl.className = "questions"
+
+
+}
