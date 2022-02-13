914c9a30c8c6bc6534ab466e1d6638827e75b160
Your Name <kalaitzidispaul@gmail.com>
2022-02-12 19:33:37 -0500

    added more functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,5 @@
 
 var questions = [{
-    
     question: "1. question 1",
     choices: ["option 1", "option 2","option 3","option 4"],
     answer: "option 1"
@@ -17,10 +16,9 @@ var questions = [{
      question: "4. question four",
      choices: ["correct answer", "option 2", "option 3", "option 4"],
      answer: "correct answer"
-}],
+}]
 
-
-//define html referneces as variables
+//define html references as variables
 var timerEl = document.querySelector('.timer')
 var questionsEl = document.querySelector('.questions')
 var startQuizBtn = document.querySelector('#start-btn')
@@ -28,7 +26,7 @@ var welcomeSlideEl = document.querySelector('.welcome-slide')
 var initalsInputEl = document.querySelector('#initials-input')
 var formEl = document.querySelector("#submit-score")
 var questionContainerEl = document.querySelector('.question-container')
-var questionTitleEl = document.querySelector('#question-name')
+var questionNameEl = document.querySelector('#question-name')
 
 //grab buttons from html and make them variables
 var choiceA = document.querySelector("#btn1");
@@ -40,14 +38,27 @@ var showAnswer = document.querySelector("#showanswer");
 //variables for quiz tracker
 var questionNumber = 0;
 var questionIndex = 0;
-var correctAnswer = 0
+var correctAnswer = 0;
 var scoreResult;
 
+//timer properties
+var counter = 75;
+var timer = setInterval(countdown, 1000);
+//timer function
+function countdown(){
+    if(counter === 0){
+        clearTimeout(timer);
+        //call high score function here
+    }else{
+        timerEl.innerHTML = "Time:" + counter;
+        counter--;
+    }
+    console.log(counter);
+};
 
-//when i click start button, timer starts and welcome slide dissapears
+//when i click start button, timer begins and welcome slide disapears
 var startQuiz = function(){
     questionIndex = 0;
-    initalsInputEl.textContent = null;
     welcomeSlideEl.setAttribute("style", "display: none;");
     formEl.setAttribute("style", "display: none;");
 
@@ -62,38 +73,56 @@ var generateQuestion = function(){
 }
 
 var nextQuestion = function(){
-    questionTitleEl.textContent = questions[questionIndex].question;
+    questionNameEl.textContent = questions[questionIndex].question;
+    questionNameEl.setAttribute("style", "color: white;")
     choiceA.textContent = questions[questionIndex].choices[0]
     choiceB.textContent = questions[questionIndex].choices[1];
     choiceC.textContent = questions[questionIndex].choices[2];
     choiceD.textContent = questions[questionIndex].choices[3];
 }
 
+var chooseA = function () {
+    checkAnswer(0);
+}
+var chooseB = function () {
+    checkAnswer(1);
+}
+var chooseC = function () {
+    checkAnswer(2);
+}
+var chooseD = function () {
+    checkAnswer(3);
+}
+
 //show if question is wrong or incorrect
 function checkAnswer(answer) {
-    if(questions[questionIndex].answer === questions[questionsIndex].choices[answer]){
+    if(questions[questionIndex].answer === questions[questionIndex].choices[answer]){
         //correct answer
         showAnswer.textContent = "Correct";
     }else {
         //wrong answer, deduct 15 seconds from timer
-         counter -= 15;
+         counter = counter - 15;
+         timerEl.textContent = counter;
+         showAnswer = "Incorrect! The Correct answer is: " + questions[questionIndex].answer;
     }
 
-}
+    questionIndex++;
+    //repeat with the rest of the questions
+    if(questionIndex < questions.length){
+        nextQuestion();
+    } 
+    checkAnswer();
+};
+//once the game is down 
 
-//timer properties
-var counter = 75;
-var timer = setInterval(countdown, 1000);
-//timer function
-function countdown(){
-    if(counter === 0){
-        clearTimeout(timer);
-    }else{
-        timerEl.innerHTML = "Time:" + counter;
-        counter--;
-    }
-}
+
 
 
 
 startQuizBtn.addEventListener("click", startQuiz);
+choiceA.addEventListener("click", chooseA);
+choiceB.addEventListener("click", chooseB);
+choiceC.addEventListener("click", chooseC);
+choiceD.addEventListener("click", chooseD);
+
+nextQuestion();
