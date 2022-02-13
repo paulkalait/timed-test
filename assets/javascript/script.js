fc684ffc6bb52793e6cf0843e934bdcbc2811691
Your Name <kalaitzidispaul@gmail.com>
2022-02-13 17:00:04 -0500

    pushing recent changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -32,6 +32,14 @@ var questionNameEl = document.querySelector('#question-name')
 var summaryEl = document.querySelector('#summary')
 var playerScoreEl = document.querySelector('#player-score')
 var submitFormEl = document.querySelector('#submit-form-btn')
+var initialsTextEl = document.querySelector('#initialsText')
+var leaderBoardEl = document.querySelector('#leaderBoard')
+var listOfHighScoresEl = document.querySelector('#listOfHighScores')
+var goBackBtnEl = document.querySelector('go-back-btn')
+var clearHighScoreEl = document.querySelector('cleer-highscore-btn')
+var viewHighScoreEl = document.querySelector('viewHighScore')
+
+
 
 //grab buttons from html and make them variables
 var choiceA = document.querySelector("#btn1");
@@ -45,7 +53,7 @@ var showAnswer = document.querySelector("#showanswer");
 var questionNumber = 0;
 var questionIndex = 0;
 var correctAnswer = 0;
-var scoreResult;
+
 
 //timer properties
 var timer
@@ -125,15 +133,75 @@ var endQuiz = function(){
     timerEl.setAttribute("style", "display: none")
     // localStorage.setItem("Highscore", counter)
 }
-var highScore = function(){
-    var result = localStorage.getItem("Highscore")
-   if(counter > result){
-       localStorage.setItem("Highscore", counter)
-   }
-}
-highScoreEl.textContent = localStorage.getItem("Highscore");
 
+var highScore = function(event){
+    event.preventDefault();
+
+    if(initialsTextEl.value === ""){
+        alert("Please enter Your initials")
+        return
+    }
+    leaderBoardEl.setAttribute("style", "display: block")
+    formEl.setAttribute("style", "display: none")
+    summaryEl.setAttribute("style", "display: none")
+    questionContainerEl.setAttribute("style", "display: none")
+    welcomeSlideEl.setAttribute("style", "display: none")
+    timerEl.setAttribute("style", "display: none")
+
+    highScoreEl = LocalStorage.setItem("Highscore", scoresArrayString)
+    var scoresArray;
+
+    if(highScoreEl === null) {
+        scoresArray = [];
+    } else{
+        scoresArray = JSON.parse(highScoreEl)
+    }
+
+    var userScore = {
+        Initials: initialsTextEl.value,
+        score: counter.textContent
+    }
+
+    console.log(userScore)
+    scoresArray.push(userScore);
+
+    //stringify array to store in local array
+    var scoresArrayString = JSON.stringify(scoresArray);
+    localStorage.getItem("Highscore")
+
+    leaderBoard();
+}
+// highScoreEl.textContent = localStorage.getItem("Highscore");
+
+//leader board section
+var i = 0
+var leaderBoard= function(){
+
+    leaderBoardEl.setAttribute("style", "display: block")
+    formEl.setAttribute("style", "display: none")
+    summaryEl.setAttribute("style", "display: none")
+    questionContainerEl.setAttribute("style", "display: none")
+    welcomeSlideEl.setAttribute("style", "display: none")
+    timerEl.setAttribute("style", "display: none")
+
+    var highScoreEl = localStorage.getItem("HighScore")
     
+
+    if(highScoreEl === null){
+        return;
+    }
+    console.log(highScoreEl);
+    
+    var storedHighScores = JSON.parse(highScoreEl);
+
+    for(; i < storedHighScores.length; i++){
+        var HighScoreList = document.createElement("li");
+        HighScoreList.textContent = storedHighScores[i].Initials + ": " + storedHighScores[i].score;
+        listOfHighScoresEl.appendChild(HighScoreList);
+
+    }
+}
+
 
 var chooseA = function () {
     checkAnswer(0);
@@ -157,6 +225,10 @@ choiceB.addEventListener("click", chooseB);
 choiceC.addEventListener("click", chooseC);
 choiceD.addEventListener("click", chooseD);
 submitFormEl.addEventListener("submit", highScore);
+viewHighScoreEl.addEventListener("click", leaderBoard)
+
+// submitFormEl.addEventListener("submit", highScore);
+
 
 
 // nextQuestion();
